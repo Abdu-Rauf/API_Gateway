24ed82908ba89444cadbbb5f94d41bd14d7b3a85
Abdu-Rauf <abdurraufaamir@gmail.com>
2026-04-09 06:17:12 +0530

    Add GET Route to Verify Tokens

--- node/server.js
+++ node/server.js
@@ -1,8 +1,35 @@
+const verifyToken = require('./verifyToken');
+const crypto = require('crypto');
+require('dotenv').config({ path: '../.env' });
 const express = require('express');
+
 const app = express();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
+
+app.get("/", async (req,res)=>{
+    // Check Wether Header Contain Bearer Token
+    const authHeader = req.headers["authorization"]
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")){
+        return res.status(401).send("Missing or Invalid Token Format")
+    }
+    const token = authHeader.split(" ")[1]
+
+    // Verify The Token
+    const decoded = verifyToken(token)
+    if (!decoded){
+        return res.status(401).send("Invalid Token Signature")
+    }else{
+        return res.status(200).send("Token Verified.")
+    }
+
+});
+
+
+// Start the server
 app.listen(PORT,()=>{
     console.log(`Server Running on port ${PORT}`)
 })
+
