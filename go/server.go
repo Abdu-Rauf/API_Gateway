217b005d67802d323158edec422e563beb6b8412
Abdu-Rauf <abdurraufaamir@gmail.com>
2026-04-16 02:15:16 +0530

    Add Redis connection and initialize Lua script for rate limiting

--- go/server.go
+++ go/server.go
@@ -44,6 +44,27 @@ func (g *Gateway) jwtHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/", jwtHandler)
+	// create the redis client
+	rdb := redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+	})
+	// Ping Redis to ensure it is connected before starting the server
+	ctx := context.Background()
+	if err := rdb.Ping(ctx).Err(); err != nil {
+		log.Fatalf("Failed to connect to Redis: %v", err)
+	}
+	log.Println("Redis client connected and ready")
+	// Load The Token Bucket Script
+	scriptPath := "../redis/token_bucket.lua"
+	scriptContent, err := os.ReadFile(scriptPath)
+	if err != nil {
+		log.Fatalf("Failed to read script file: %v", err)
+	}
+	// Start the server
+	app := &Gateway{
+		redisClient: rdb,
+		rtlScript:   redis.NewScript(string(scriptContent)),
+	}
+	http.HandleFunc("/", app.jwtHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
